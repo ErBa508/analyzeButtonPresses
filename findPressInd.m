b720837ec10af135a545fac6aa360496c92722d8
Erin Babinsky <ErBa508@users.noreply.github.com>
2015-03-27 15:15:48 +0100

    small edits to findPressInd.m var names

--- findPressInd.m
+++ findPressInd.m
@@ -1,6 +1,9 @@
-function [indStart, indEnd] = findPressInd(vecStart,vecEnd, i, TC1)
+function [indStart, indEnd] = findPressInd(vecPressOn,vecPressOff, i, TC1, FR)
 
-tmp1 = TC1(vecStart(i),2); % retrieve time of press on
-tmp2 = TC1(vecEnd(i),2); % retrieve time of press off
-indStart = round(tmp1/(1/120)); % transform time in sec to frame # (index #)
-indEnd = round(tmp2/(1/120));
+%keyboard
+
+tmp1 = TC1(vecPressOn(i),2); % retrieve time of press on
+tmp2 = TC1(vecPressOff(i),2); % retrieve time of press off
+indStart = round(tmp1/(1/FR)); % transform time in sec to frame # (index #)
+indEnd = round(tmp2/(1/FR));
+
