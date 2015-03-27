4304c3bc39b179459994f83f0fb39e4a30d0c0d0
Erin Babinsky <ErBa508@users.noreply.github.com>
2015-03-27 15:18:01 +0100

    incorporate lastDuration.m function into genTimeSeries.m

--- genTimeSeries.m
+++ genTimeSeries.m
@@ -1,30 +1,39 @@
 function [timeSeries] = genTimeSeries(TC,FR)
 
-% find time of last button press/release
+ % find time of last button press/release
  timeMaxTC = max(TC(:,2)); 
  
  % make new timeSeries vector with resolution of 120 Hz
  timeSeries = (0:1/FR:timeMaxTC)'; 
+ timeMaxTS = max(timeSeries(:,1));
  
  % add a second column of zeros to new timeSeries vector
  timeSeries(:,2:3) = zeros(size(timeSeries,1),2);
  
  % make a vector with the indices of (a) press Aon and (b) pressAoff
- indAStart = find(TC(:,3) == 1);
- indAEnd = find(TC(:,3) == 2);
+ indA_ON = find(TC(:,3) == 1);
+ indA_OFF = find(TC(:,3) == 2);
  
  % make a vector with the indices of (a) press Bon and (b) pressBoff
- indBStart = find(TC(:,3) == -1);
- indBEnd = find(TC(:,3) == -2);
+ indB_ON = find(TC(:,3) == -1);
+ indB_OFF = find(TC(:,3) == -2);
+ 
+ % check whether last button press of trial is still "on"; e.g. was not
+ % released when trial ended. This will make lengths of ind_ON vs ind_OFF
+ % unequal. We can fix that by adding a dummy "off" press in last frame of
+ % the trial (we add this new event to the time course (TC)).
+ [indA_ON, indA_OFF, indB_ON, indB_OFF, TC] = lastDuration(indA_ON, indA_OFF, indB_ON, indB_OFF, TC, timeMaxTS);
  
  % loop through vector of keypress indices to get frames where start  
  % press and end press. Then mark all frames in between as the corresponding
  % press A or B.
- for i = 1: length(indAEnd)
+ for i = 1: length(indA_OFF)
      
-     [indAStartVal, indAEndVal] = findPressInd(indAStart,indAEnd, i, TC);
+     [indAStartVal, indAEndVal] = findPressInd(indA_ON,indA_OFF, i, TC, FR);
      timeSeries(indAStartVal:indAEndVal, 2) = 1; % use indices to mark when press Aon (col 2)
      
-     [indBStartVal, indBEndVal] = findPressInd(indBStart,indBEnd, i, TC);
+     [indBStartVal, indBEndVal] = findPressInd(indB_ON,indB_OFF, i, TC, FR);
      timeSeries(indBStartVal:indBEndVal, 3) = 1; % use indices to mark when press Bon (col 3)
  end
+ 
+ 
--- lastDuration.m
+++ lastDuration.m
@@ -1,9 +1,15 @@
-function [vecAon, vecAoff, vecBon, vecBoff] = lastDuration(vecAon, vecAoff, vecBon, vecBoff, Tmax, epsilon)
+function [vecAon, vecAoff, vecBon, vecBoff, TC] = lastDuration(vecAon, vecAoff, vecBon, vecBoff, TC, timeMaxTS)
 
+%keyboard
+
+len_TC = length(TC(:,1));
+TC(len_TC + 1, 1) = len_TC + 1; % add new index to time course column 1
+TC(len_TC + 1, 2) = timeMaxTS; % set last 'press' in column 2 to last frame of time series
 
 % Button A
 if (length(vecAon)-length(vecAoff))==1 %end of trial occurred during Aon
-    vecAoff(length(vecAoff)+1)=Tmax-epsilon;   % to use find(t(..)=f(epsilon)) etc
+    vecAoff(length(vecAoff)+1) = vecAon(length(vecAon) + 1);   % add new index to vecAoff
+    TC(len_TC + 1, 3) = 2; % set event label in column 3 to Aoff
 else
     if (length(vecAon)~=length(vecAoff)) %if end of trial was during Aoff,
         fprintf('Warning: Aon and Aoff should be of equal length\n')            
@@ -18,7 +24,8 @@ end
 
 % Button B
 if (length(vecBon)-length(vecBoff))==1
-    vecBoff(length(vecBoff)+1)=Tmax-epsilon;
+    vecBoff(length(vecBoff)+1)= vecBon(length(vecBon)) + 1; % add new index to vecBoff
+    TC(len_TC + 1, 3) = -2; % set event label in column 3 to Boff
 else
     if (length(vecBon)~=length(vecBoff))
         fprintf('Warning: Bon and Boff should be of equal length\n')
