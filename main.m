c5fe36840984b46cc64b0e5be8ceb66dd44f4ab3
Erin Babinsky <ErBa508@users.noreply.github.com>
2015-03-31 16:32:29 +0200

    main.m: re-arrange and therefore combine summarize data sections for analyses 1 and 2

--- main.m
+++ main.m
@@ -3,34 +3,44 @@
 % Modified from NR analyzeTC.m function
 
 
-%% Clear workspace
+%%%%%%%%%%%%%%%%%%%%%
+%% Clear workspace %%
+%%%%%%%%%%%%%%%%%%%%%
 
 clc
 clear all
 close all
 
-%% Constant
 
-AnalysisType = 1; %if single file analysis = 1; if batch analysis = 2; if simulation = 3
+
+%%%%%%%%%%%%%%%
+%% CONSTANTS %%
+%%%%%%%%%%%%%%%
+
+AnalysisType = 2; %if single file analysis = 1; if batch analysis = 2; if simulation = 3
 
 FR = 120; % frame rate
 
 plot_yn = 1; % if want to see summary plots (1 if yes, 0 if no)
 
-%% Option (1) SINGLE file analysis
 
-if AnalysisType == 1 
-    
+
+%%%%%%%%%%%%%%
+%% GET DATA %%
+%%%%%%%%%%%%%%
+
+if AnalysisType == 1
     %%%%%%%%%%%%%%%%%%%
     %%% Select data %%%
     %%%%%%%%%%%%%%%%%%%
-      
+    
     [filename, pathname] = uigetfile('C:\Users\Erin\Box Sync\UPF\PlaidProj\Data\raw\2013 ButtonPress dat files\*.dat', 'Pick a .dat data file');
     rawData = importdata(strcat(pathname, filename));
+    numFiles = 1;
     
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%% Save data to mat file %%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%% Save raw data to mat file %%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     % See if data already saved to mat file %
     load('C:\Users\Erin\Box Sync\UPF\PlaidProj\Data\processed\keyPressData\keyData.mat');
@@ -40,11 +50,38 @@ if AnalysisType == 1
         fileRepeat(i,1) =  strcmp(keyData.subjects(1,i).name, filename);
     end
     
-    % Save data to mat file %
+    % Save raw data to mat file %
     if sum(fileRepeat) < 1
         keyData.subjects(1, L + 1).name = filename;
         keyData.subjects(1, L + 1).data = rawData;
         save('C:\Users\Erin\Box Sync\UPF\PlaidProj\Data\processed\keyPressData\keyData.mat','keyData');
+    end
+    
+elseif AnalysisType == 2
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%% Get raw data mat file %%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    load('C:\Users\Erin\Box Sync\UPF\PlaidProj\Data\processed\keyPressData\keyData.mat');
+    numFiles = length(keyData.subjects);
+end
+
+
+
+%%%%%%%%%%%%%%%%%%%%
+%% SUMMARIZE DATA %%
+%%%%%%%%%%%%%%%%%%%%
+
+for i = 1 : numFiles
+    
+    if AnalysisType == 2      
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%% Select raw data from mat file %%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        clear rawData; clear gapOverlap; % clear data from prev loop
+        rawData = keyData.subjects(1,i).data;
+        filename = keyData.subjects(i).name;
     end
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -64,7 +101,7 @@ if AnalysisType == 1
     %%% Clean-up gaps and overlaps %%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
-    timeSeriesTC1 = cleanUpTC(timeSeriesTC1);
+    timeSeriesTC1 = cleanUpTS(timeSeriesTC1);
         
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%% Summarize and visualize time series POST-clean-up %%%
@@ -72,28 +109,24 @@ if AnalysisType == 1
 
     [gapOverlap_post, meanGapOverlap_post, stdGapOverlap_post, durA_post, durB_post] = summarizeData(timeSeriesTC1, filename, plot_yn);
     
-%% Option (2) BATCH analysis    
-elseif AnalysisType == 2 %if > 0 then run batch analysis
-
-    load('C:\Users\Erin\Box Sync\UPF\PlaidProj\Data\processed\keyPressData\keyData.mat');
-    %load('keyRes.mat');
+    pause % to view histogram and plot for each subject
     
-    for i = 1 : length(keyData.subjects)
-        clear rawData; clear gapOverlap; % clear data from prev loop
-        rawData = keyData.subjects(1,i).data;
-        [gapOverlap, meanGapOverlap, stdGapOverlap, durA, durB] = analyzePress(rawData,  keyData.subjects(i).name);
+    %if AnalysisType == 2
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%% Save result data to mat file %%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
-        % save key press RESULTS to different .mat file %
-        keyRes.subjects(1,i).name = keyData.subjects(1, i).name; %save filename
-        keyRes.subjects(1,i).gap = gapOverlap; %save gaps/overlaps values
-        keyRes.subjects(1,i).results(1) = meanGapOverlap; %save single val results in results bin
-        keyRes.subjects(1,i).results(2) = stdGapOverlap;
-        save('C:\Users\Erin\Box Sync\UPF\PlaidProj\Data\processed\keyPressData\keyRes.mat','keyRes');
-        
-        %pause % to view histogram and plot for each subject
-    end
+        %         %save key press RESULTS to different .mat file %
+        %         keyRes.subjects(1,i).name = keyData.subjects(1, i).name; %save filename
+        %         keyRes.subjects(1,i).gap = gapOverlap; %save gaps/overlaps values
+        %         keyRes.subjects(1,i).results(1) = meanGapOverlap; %save single val results in results bin
+        %         keyRes.subjects(1,i).results(2) = stdGapOverlap;
+        %         save('C:\Users\Erin\Box Sync\UPF\PlaidProj\Data\processed\keyPressData\keyRes.mat','keyRes');
+    %end
     
 end
+
+
 
 %% Option (3) SIMULATION
 
@@ -122,27 +155,27 @@ end
 
 %% Evaluate group data if real data
 
-if AnalysisType == 2
-    
-    load('C:\Users\Erin\Box Sync\UPF\PlaidProj\Data\processed\keyPressData\keyRes.mat','keyRes');
-    
-    for i = 1 : length(keyData.subjects)
-        groupMeanGap(i,1) = keyRes.subjects(1,i).results(1);
-        groupMeanStd(i,1) = keyRes.subjects(1,i).results(2);
-    end
-    
-    
-    figure
-    hist(groupMeanGap)
-    str = sprintf('Mean gap time between press for %d subjects with mean gap %.2f \n (a negative # would indicate overlap)', length(groupMeanGap), mean(groupMeanGap));
-    title(str)
-    figure
-    hist(groupMeanStd)
-    str2 = sprintf('Standard deviation for gap time for %d subjects with mean SD %.2f', length(groupMeanStd), mean(groupMeanStd));
-    title(str2)
-    
-    % ~ 120 ms gap is the mean, ~ 50 is 1 SD : anything > 220 ms is an 'outlier'
-end
+% if AnalysisType == 2
+%     
+%     load('C:\Users\Erin\Box Sync\UPF\PlaidProj\Data\processed\keyPressData\keyRes.mat','keyRes');
+%     
+%     for i = 1 : length(keyData.subjects)
+%         groupMeanGap(i,1) = keyRes.subjects(1,i).results(1);
+%         groupMeanStd(i,1) = keyRes.subjects(1,i).results(2);
+%     end
+%     
+%     
+%     figure
+%     hist(groupMeanGap)
+%     str = sprintf('Mean gap time between press for %d subjects with mean gap %.2f \n (a negative # would indicate overlap)', length(groupMeanGap), mean(groupMeanGap));
+%     title(str)
+%     figure
+%     hist(groupMeanStd)
+%     str2 = sprintf('Standard deviation for gap time for %d subjects with mean SD %.2f', length(groupMeanStd), mean(groupMeanStd));
+%     title(str2)
+%     
+%     %~ 120 ms gap is the mean, ~ 50 is 1 SD : anything > 220 ms is an 'outlier'
+% end
 
 %% Evaluate data if simulated
 
