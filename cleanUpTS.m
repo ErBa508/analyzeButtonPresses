6ca723eaff766e1a9c85cd6be226b45a022fb65a
Erin Babinsky <ErBa508@users.noreply.github.com>
2015-04-17 16:45:59 +0200

    Bug fix: now cleans up overlaps at end of trial

--- cleanUpTS.m
+++ cleanUpTS.m
@@ -1,14 +1,31 @@
 function [TS] = cleanUpTS(TS)
 
+%keyboard 
+
 % find indices when col 2 (press A) and col 3 (press B) are both pressed
 % (== 1)
 overlapInd = find(TS(:,2) + TS(:,3) == 2); 
 
 for i = length(overlapInd):-1:1 % loop backward (forward will not work for consec overlaps)
-    if TS(overlapInd(i) + 1 , 2) == 0 % if button A in frame i + 1 == 0
-        TS(overlapInd(i), 2) = 0; % button A in frame i should = 0
+    
+    % if the trial ends with both buttons pressed
+    if max(overlapInd) == length(TS) 
+        bin = find(TS(:,2) + TS(:,3) == 1, 1, 'last'); % find the last non-overlapping frame
+        
+        if TS(bin, 2) == 0 % if button A is not pressed right before the overlap
+           TS(bin + 1 : length(TS), 3) = 0; % set button B to 0 in the last frames
+        else
+           TS(bin + 1 : length(TS), 2) = 0; % else set button A to 0 in the last frames
+        end
+        
+    % if the trial ends with only one button pressed
     else
-        TS(overlapInd(i), 3) = 0; % else button B in frame i should = 0
+        
+        if TS(overlapInd(i) + 1 , 2) == 0 % if button A in frame i + 1 == 0
+            TS(overlapInd(i), 2) = 0; % button A in frame i should = 0
+        else
+            TS(overlapInd(i), 3) = 0; % else button B in frame i should = 0
+        end
     end
 end
 
