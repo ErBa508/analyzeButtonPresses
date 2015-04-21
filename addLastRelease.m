4cc30c51752442c5568e614f6f88eca1f66e54c4
Erin Babinsky <ErBa508@users.noreply.github.com>
2015-04-21 11:57:01 +0200

    Change TC to ES in addLastRelease.m, e.g. from 'time course to event series'

--- addLastRelease.m
+++ addLastRelease.m
@@ -1,16 +1,16 @@
-function [vecAoff, vecBoff, TC] = addLastRelease(vecAon, vecAoff, vecBon, vecBoff, TC, timeMaxTS)
+function [vecAoff, vecBoff, ES] = addLastRelease(vecAon, vecAoff, vecBon, vecBoff, ES, timeMaxTS)
 
 %keyboard
 
-len_TC = length(TC(:,1));
+len_ES = length(ES(:,1)); % length of Event Series
 
 % Button A
 if (length(vecAon)-length(vecAoff))==1 %end of trial occurred during Aon
     vecAoff(length(vecAoff)+1, 1) = vecAon(length(vecAon)) + 1;   % add new index to vecAoff
-    % add new row to TC
-    TC(len_TC + 1, 1) = len_TC + 1; % add new index to time course column 1
-    TC(len_TC + 1, 2) = timeMaxTS; % set last 'press' in column 2 to last frame of time series
-    TC(len_TC + 1, 3) = 2; % set event label in column 3 to Aoff
+    % add new row to ES
+    ES(len_ES + 1, 1) = len_ES + 1; % add new index to time course column 1
+    ES(len_ES + 1, 2) = timeMaxTS; % set last 'press' in column 2 to last frame of time series
+    ES(len_ES + 1, 3) = 2; % set event label in column 3 to Aoff
 else
     if (length(vecAon)~=length(vecAoff)) %if end of trial was during Aoff,
         fprintf('Warning: Aon and Aoff should be of equal length\n')            
@@ -26,10 +26,10 @@ end
 % Button B
 if (length(vecBon)-length(vecBoff))==1
     vecBoff(length(vecBoff)+1, 1)= vecBon(length(vecBon)) + 1; % add new index to vecBoff
-    % add new row to TC
-    TC(len_TC + 1, 1) = len_TC + 1; % add new index to time course column 1
-    TC(len_TC + 1, 2) = timeMaxTS; % set last 'press' in column 2 to last frame of time series
-    TC(len_TC + 1, 3) = -2; % set event label in column 3 to Boff
+    % add new row to ES
+    ES(len_ES + 1, 1) = len_ES + 1; % add new index to time course column 1
+    ES(len_ES + 1, 2) = timeMaxTS; % set last 'press' in column 2 to last frame of time series
+    ES(len_ES + 1, 3) = -2; % set event label in column 3 to Boff
 else
     if (length(vecBon)~=length(vecBoff))
         fprintf('Warning: Bon and Boff should be of equal length\n')
