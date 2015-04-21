db26a7b5794cc7f62db9f2190250785604142415
Erin Babinsky <ErBa508@users.noreply.github.com>
2015-04-21 11:58:37 +0200

    Change TC to ES in findPressInd.m

--- findPressInd.m
+++ findPressInd.m
@@ -1,9 +1,9 @@
-function [indStart, indEnd] = findPressInd(vecPressOn,vecPressOff, i, TC, FR)
+function [indStart, indEnd] = findPressInd(vecPressOn,vecPressOff, i, ES, FR)
 
 %keyboard
 
-tmp1 = TC(vecPressOn(i),2); % retrieve time of press on
-tmp2 = TC(vecPressOff(i),2); % retrieve time of press off
+tmp1 = ES(vecPressOn(i),2); % retrieve time of press on
+tmp2 = ES(vecPressOff(i),2); % retrieve time of press off
 indStart = round(tmp1/(1/FR)); % transform time in sec to frame # (index #)
 indEnd = round(tmp2/(1/FR));
 
