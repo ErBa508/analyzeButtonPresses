6f0c71854164eaf81ea0a93fa376b01c65f35299
Erin Babinsky <ErBa508@users.noreply.github.com>
2015-05-18 17:43:00 +0200

    Bug fix: account for instance when only one button pressed

--- summarizeData.m
+++ summarizeData.m
@@ -1,6 +1,6 @@
 function [gapOverlap, meanGapOverlap, stdGapOverlap, durA, durB, numSwitches] = summarizeData(TS, filename, plot_yn, startTime)
 
-%keyboard
+keyboard
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Summarize gaps and overlaps %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -38,16 +38,22 @@ timesCat = cat(1,timesA_split, timesB_split);
 timesSort = timesCat(d2,:);
 numSwitches = size(timesSort,1) - 1; % -1 because # of switches is one less than # of presses
 
-% measure gaps and overlaps
-for i = 1: numSwitches 
-    gapOverlap(i,1) = timesSort(i+1,1) - timesSort(i,2); % next press minus previous button release
-    gapOverlap(i,2) = timesSort(i+1,3) - timesSort(i,3); % gap between same button? if = +/-2 there is a button switch, if = 0 gap between same button
-    
-end
+if numSwitches > 0
+    % measure gaps and overlaps
+    for i = 1: numSwitches 
+        gapOverlap(i,1) = timesSort(i+1,1) - timesSort(i,2); % next press minus previous button release
+        gapOverlap(i,2) = timesSort(i+1,3) - timesSort(i,3); % gap between same button? if = +/-2 there is a button switch, if = 0 gap between same button
 
-%disp('gapOverlap var: positive value = gap, negative value = overlap');
-meanGapOverlap = mean(gapOverlap(:,1));
-stdGapOverlap = std(gapOverlap(:,1));
+    end
+
+    %disp('gapOverlap var: positive value = gap, negative value = overlap');
+    meanGapOverlap = mean(gapOverlap(:,1));
+    stdGapOverlap = std(gapOverlap(:,1));
+else
+    gapOverlap = [0,0];
+    meanGapOverlap = 0;
+    stdGapOverlap = 0;
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Summarize dominance durations %%%
@@ -55,6 +61,13 @@ stdGapOverlap = std(gapOverlap(:,1));
 
 durA = timesA_split(:,2) - timesA_split(:,1);
 durB = timesB_split(:,2) - timesB_split(:,1);
+
+% account for possibility that button A or B were never pressed
+if isempty(durA)
+    durA = 0;
+elseif isempty(durB)
+    durB = 0;
+end
 
 %%%%%%%%%%%%%%%%%%%%%%
 %%% Visualize data %%%
@@ -77,7 +90,7 @@ if plot_yn==1
     hold on
     plotTC(h1,timesB_split(:,1:2),startTime, Tmax,YvalsB,colorB)
     
-    str = sprintf('Filename is "%s" and # of presses = %d', strrep(filename, '_', ' '), length(timesCat)); %need to take out underscores or formats text as subscript in title
+    str = sprintf('Filename is "%s" and # of presses = %d', strrep(filename, '_', ' '), size(timesCat,1)); %need to take out underscores or formats text as subscript in title
     title(str)
     
     %%% Histogram of dominance durations %%%
@@ -98,28 +111,28 @@ if plot_yn==1
     end
     
     %%% Histogram of gaps and overlaps
-    
+            
     tmpmin = min(gapOverlap(:,1));
     tmpmax = max(gapOverlap(:,1));
-    
+
     if tmpmax == 0 || tmpmin == tmpmax % if no overlaps in event series, or if min == max, 
         tmpmax = 0.6; % set arbitrary value for tmax 
     end % this will be used as the max x-axis val in the hist
-    
+
     binw2 = (abs(tmpmin)+tmpmax)/6;
-    
+
     bins2 = tmpmin: binw2: tmpmax;
     if bins2(end) < tmpmax
         bins2(end + 1) = bins2(end) + binw2;
     end
-    
+
     hist = histc(gapOverlap, bins2);
     h3 = subplot(3,1,3);
     %figure
     bar(bins2, hist, 'EdgeColor', colorA, 'FaceColor', colorA)
     str = sprintf('Gaps(+) and overlaps(-);\n Bin width %.2f s and and no bin shift\n Mean gap/overlap = %.3f s', binw2, meanGapOverlap);
     title(str)
-    
+        
 end
 
 %keyboard
